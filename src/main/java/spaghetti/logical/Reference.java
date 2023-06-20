833cdb6ecb307fce798aa22fae6fd51442c74ad2
Logical Spaghetti <130314555+LogicalSpaghetti@users.noreply.github.com>
2023-06-20 11:49:41 -0400

    Add files via upload

---
+++ src/main/java/spaghetti/logical/Reference.java
@@ -0,0 +1,8 @@
+package spaghetti.logical;
+
+public class Reference {
+    public static final String MODID = "logic";
+    public static final String MODNAME = "Logical Mod";
+    public static final String VERSION = "1.0";
+    public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
+}
---
+++ src/main/java/spaghetti/logical/TutorialMod.java
@@ -0,0 +1,33 @@
+package spaghetti.logical;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import spaghetti.logical.init.ModItems;
+
+@Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION, acceptedMinecraftVersions=Reference.ACCEPTED_MINECRAFT_VERSIONS)
+
+public class TutorialMod {
+
+    @Instance
+    public static TutorialMod instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        System.out.println(Reference.MODID + ":preInit");
+        ModItems.init();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println(Reference.MODID + ":init");
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        System.out.println(Reference.MODID + ":postInit");
+    }
+}
